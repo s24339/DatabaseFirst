791fff93095b99a26388f982f8297e185d5e52e5
s24339 <s24339@pjwstk.edu.pl>
2024-05-27 01:00:55 +0200

    Initial Commit

--- DatabaseFirst/obj/Debug/net8.0/DatabaseFirst.AssemblyInfo.cs
+++ DatabaseFirst/obj/Debug/net8.0/DatabaseFirst.AssemblyInfo.cs
@@ -13,7 +13,7 @@ using System.Reflection;
 [assembly: System.Reflection.AssemblyCompanyAttribute("DatabaseFirst")]
 [assembly: System.Reflection.AssemblyConfigurationAttribute("Debug")]
 [assembly: System.Reflection.AssemblyFileVersionAttribute("1.0.0.0")]
-[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0")]
+[assembly: System.Reflection.AssemblyInformationalVersionAttribute("1.0.0+f617ec6e7423cf367883ca4d396b1a1fcaca35ea")]
 [assembly: System.Reflection.AssemblyProductAttribute("DatabaseFirst")]
 [assembly: System.Reflection.AssemblyTitleAttribute("DatabaseFirst")]
 [assembly: System.Reflection.AssemblyVersionAttribute("1.0.0.0")]
